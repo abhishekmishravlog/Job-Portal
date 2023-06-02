d57c66c5b2fd4ddd5a2858b62fe0537bc03f5f5c
Code With Anurag <anurag.am2003@gmail.com>
2023-06-03 00:12:56 +0530

    connected to db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors=require('cors')
+const dotenv=require('dotenv')
+
+// MongoDB
+mongoose.set('strictQuery', true);
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL, {
+  dbName: "JobPortal",
+}).then(() => {
+  console.log("DB Connected");
+}).catch((err) => {
+  console.log(err);
+});
 
 const app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// Setting up middlewares
+app.use(cors());
+app.use(express.json());
 
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
