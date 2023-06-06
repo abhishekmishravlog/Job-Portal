7cf3252a4b4f45dce9a30e86bf6f598ab112cc19
abhishekmishravlog <abhishek.mish2002@gmail.com>
2023-06-06 23:44:38 +0530

    job creation form added

--- client/src/lib/apiList.js
+++ client/src/lib/apiList.js
@@ -3,6 +3,7 @@ export const server = "http://localhost:4444";
 const apiList = {
   login: `${server}/auth/login`,
   signup: `${server}/auth/signup`,
+  jobs: `${server}/api/jobs`,
 };
 
 export default apiList;
