b52087837d7dcd216afd87d9acd6e4010630ab72
Code With Anurag <anurag.am2003@gmail.com>
2023-06-11 00:12:57 +0530

    applicant can apply for a job

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -6,6 +6,7 @@ const User = require("../db/User");
 const JobApplicant = require("../db/JobApplicant");
 const Recruiter = require("../db/Recruiter");
 const Job = require("../db/Job");
+const Application = require("../db/Application")
 
 const router = express.Router();
 
@@ -89,6 +90,122 @@ router.get("/jobs", jwtAuth, (req, res) => {
     })
     .catch((err) => {
       res.status(400).json(err);
+    });
+});
+
+// apply for a job 
+router.post("/jobs/:id/applications", jwtAuth, (req, res) => {
+  const user = req.user;
+  if (user.type != "applicant") {
+    res.status(401).json({
+      message: "You don't have permissions to apply for a job",
+    });
+    return;
+  }
+  const data = req.body;
+  const jobId = req.params.id;
+
+  // check whether applied previously
+  // find job
+  // check count of active applications < limit
+  // check user had < 10 active applications && check if user is not having any accepted jobs (user id)
+  // store the data in applications
+
+  Application.findOne({
+    userId: user._id,
+    jobId: jobId,
+    status: {
+      $nin: ["deleted", "accepted", "cancelled"],
+    },
+  })
+    .then((appliedApplication) => {
+      console.log(appliedApplication);
+      if (appliedApplication !== null) {
+        res.status(400).json({
+          message: "You have already applied for this job",
+        });
+        return;
+      }
+
+      Job.findOne({ _id: jobId })
+        .then((job) => {
+          if (job === null) {
+            res.status(404).json({
+              message: "Job does not exist",
+            });
+            return;
+          }
+          Application.countDocuments({
+            jobId: jobId,
+            status: {
+              $nin: ["rejected", "deleted", "cancelled", "finished"],
+            },
+          })
+            .then((activeApplicationCount) => {
+              if (activeApplicationCount < job.maxApplicants) {
+                Application.countDocuments({
+                  userId: user._id,
+                  status: {
+                    $nin: ["rejected", "deleted", "cancelled", "finished"],
+                  },
+                })
+                  .then((myActiveApplicationCount) => {
+                    if (myActiveApplicationCount < 10) {
+                      Application.countDocuments({
+                        userId: user._id,
+                        status: "accepted",
+                      }).then((acceptedJobs) => {
+                        if (acceptedJobs === 0) {
+                          const application = new Application({
+                            userId: user._id,
+                            recruiterId: job.userId,
+                            jobId: job._id,
+                            status: "applied",
+                            sop: data.sop,
+                          });
+                          application
+                            .save()
+                            .then(() => {
+                              res.json({
+                                message: "Job application successful",
+                              });
+                            })
+                            .catch((err) => {
+                              res.status(400).json(err);
+                            });
+                        } else {
+                          res.status(400).json({
+                            message:
+                              "You already have an accepted job. Hence you cannot apply.",
+                          });
+                        }
+                      });
+                    } else {
+                      res.status(400).json({
+                        message:
+                          "You have 10 active applications. Hence you cannot apply.",
+                      });
+                    }
+                  })
+                  .catch((err) => {
+                    res.status(400).json(err);
+                  });
+              } else {
+                res.status(400).json({
+                  message: "Application limit reached",
+                });
+              }
+            })
+            .catch((err) => {
+              res.status(400).json(err);
+            });
+        })
+        .catch((err) => {
+          res.status(400).json(err);
+        });
+    })
+    .catch((err) => {
+      res.json(400).json(err);
     });
 });
 
